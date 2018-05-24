cd07b37a22396b4f7fc08d89949bba683be13332
seanccc <sean831004@gmail.com>
2018-05-24 20:04:43 +0800

    first commit

---
+++ src/csvInput.py
@@ -0,0 +1,9 @@
+import csv
+import sys
+
+# dir define
+train_file_dir = './res/oriFile/train.csv'
+train_file_obj = open(train_file_dir,"r")
+reader = csv.reader(train_file_obj)
+for item in reader:
+    print(item[2])
